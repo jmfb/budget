da5108577491c446889660d31b23f75c77c6c35b
Jacob Buysse <Jacob.Buysse@gmail.com>
2022-05-21 15:56:51 -0500

    Scaling statistics to max of over/under for entire range for both over and under.

--- client/src/pages/Statistics/Week.tsx
+++ client/src/pages/Statistics/Week.tsx
@@ -13,27 +13,29 @@ export default function Week({
 	maxUnderBudget,
 	maxOverBudget
 }: IWeekProps) {
+	const range = Math.max(-maxOverBudget, maxUnderBudget, 1);
+	const overage = -remainingBudget;
 	return (
 		<div>
-			{remainingBudget > 0 &&
+			{remainingBudget >= 0 &&
 				<div className={styles.row}>
 					<div className={styles.overBox}>
 						<div className={styles.overLabel}>Under {budgetService.format(remainingBudget)}</div>
 					</div>
 					<div className={styles.underBox}>
 						<div className={styles.under} style={{ flexGrow: remainingBudget }} />
-						<div className={styles.notUnder} style={{ flexGrow: maxUnderBudget - remainingBudget }} />
+						<div className={styles.notUnder} style={{ flexGrow: range - remainingBudget }} />
 					</div>
 				</div>
 			}
 			{remainingBudget < 0 &&
 				<div className={styles.row}>
 					<div className={styles.overBox}>
-						<div className={styles.notOver} style={{ flexGrow: -maxOverBudget + remainingBudget }} />
-						<div className={styles.over} style={{ flexGrow: -remainingBudget }} />
+						<div className={styles.notOver} style={{ flexGrow: range - overage }} />
+						<div className={styles.over} style={{ flexGrow: overage }} />
 					</div>
 					<div className={styles.underBox}>
-						<div className={styles.underLabel}>Over {budgetService.format(-remainingBudget)}</div>
+						<div className={styles.underLabel}>Over {budgetService.format(overage)}</div>
 					</div>
 				</div>
 			}
