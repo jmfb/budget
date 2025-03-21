bdf77256ee5dd9f0a482b9e969b5415670a35ff4
Jacob Buysse <Jacob.Buysse@gmail.com>
2025-03-21 07:20:07 -0500

    Adding transaction SPs.

---
+++ dbup/Scripts/StoredProcedures/transaction_u.sql
@@ -0,0 +1,31 @@
+create or replace function budget.transaction_u (
+	p_id int,
+	p_date date,
+	p_source_id int,
+	p_raw_text text,
+	p_amount money,
+	p_original_category text,
+	p_category_id int,
+	p_note text,
+	p_expense_id int,
+	p_income_id int
+)
+returns void
+language plpgsql security definer
+as $$ begin
+
+update budget.transactions as transactions_update
+set
+	"date" = p_date,
+	source_id = p_source_id,
+	raw_text = p_raw_text,
+	amount = p_amount,
+	original_category = p_original_category,
+	category_id = p_category_id,
+	note = p_note,
+	expense_id = p_expense_id,
+	income_id = p_income_id
+where
+	transactions_update.id = p_id;
+
+end $$;
---
+++ dbup/Scripts/StoredProcedures/transactions_s.sql
@@ -0,0 +1,44 @@
+create or replace function budget.transactions_s (
+	p_start_date_inclusive date,
+	p_end_date_exclusive date,
+	p_skip int,
+	p_take int
+)
+returns table (
+	id int,
+	"date" date,
+	source_id int,
+	raw_text text,
+	amount money,
+	original_category text,
+	category_id int,
+	note text,
+	expense_id int,
+	income_id int
+)
+language plpgsql security definer
+as $$ begin return query
+
+select
+	transactions.id,
+	transactions."date",
+	transactions.source_id,
+	transactions.raw_text,
+	transactions.amount,
+	transactions.original_category,
+	transactions.category_id,
+	transactions.note,
+	transactions.expense_id,
+	transactions.income_id
+from budget.transactions as transactions
+where
+	transactions."date" >= p_start_date_inclusive
+	and transactions."date" < p_end_date_exclusive
+order by
+	transactions."date" asc,
+	transactions.amount asc,
+	transactions.id asc
+limit p_take
+offset p_skip;
+
+end $$;
