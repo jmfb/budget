4799eb9aa35e75ca3c093d8a8c717cd4566342b0
Jacob Buysse <Jacob.Buysse@gmail.com>
2025-03-22 12:47:59 -0500

    Adding new controllers.

--- server/Contracts/RequestContracts/RetireCategoryRequest.cs
+++ server/Contracts/RequestContracts/RetireCategoryRequest.cs
@@ -1,1 +1,7 @@
 namespace Budget.Server.Contracts.RequestContracts;
+
+public class RetireCategoryRequest
+{
+	public int RetireId { get; set; }
+	public int ReplacementId { get; set; }
+}
---
+++ server/Controllers/PendingItemsController.cs
@@ -0,0 +1,91 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Budget.Server.Contracts.RequestContracts;
+using Budget.Server.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budget.Server.Controllers;
+
+[Authorize]
+[Route("pending-items")]
+public class PendingItemsController(IPendingItemDataBridge dataBridge)
+	: Controller
+{
+	[HttpGet]
+	public async Task<IActionResult> GetAllAsync(
+		CancellationToken cancellationToken
+	)
+	{
+		var pendingItems = await dataBridge.GetAllAsync(cancellationToken);
+		return Ok(pendingItems);
+	}
+
+	[HttpGet("{id:int}")]
+	public async Task<IActionResult> GetAsync(
+		[FromRoute] int id,
+		CancellationToken cancellationToken
+	)
+	{
+		var pendingItem = await dataBridge.GetAsync(id, cancellationToken);
+		if (pendingItem == null)
+			return NotFound();
+		return Ok(pendingItem);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> CreateAsync(
+		[FromBody] CreatePendingItemRequest request,
+		CancellationToken cancellationToken
+	)
+	{
+		if (request == null)
+			return BadRequest("Body is required");
+		var id = await dataBridge.CreateAsync(
+			request.Name,
+			request.Amount,
+			request.CategoryId,
+			request.ExpenseId,
+			request.IncomeId,
+			cancellationToken
+		);
+		return CreatedAtAction(nameof(GetAsync), new { id }, id);
+	}
+
+	[HttpPut("{id:int}")]
+	public async Task<IActionResult> UpdateAsync(
+		[FromRoute] int id,
+		[FromBody] UpdatePendingItemRequest request,
+		CancellationToken cancellationToken
+	)
+	{
+		if (request == null)
+			return BadRequest("Body is required");
+		var pendingItem = await dataBridge.GetAsync(id, cancellationToken);
+		if (pendingItem == null)
+			return NotFound();
+		await dataBridge.UpdateAsync(
+			id,
+			request.Name,
+			request.Amount,
+			request.CategoryId,
+			request.ExpenseId,
+			request.IncomeId,
+			cancellationToken
+		);
+		return NoContent();
+	}
+
+	[HttpDelete("{id:int}")]
+	public async Task<IActionResult> DeleteAsync(
+		[FromRoute] int id,
+		CancellationToken cancellationToken
+	)
+	{
+		var pendingItem = await dataBridge.GetAsync(id, cancellationToken);
+		if (pendingItem == null)
+			return NotFound();
+		await dataBridge.DeleteAsync(id, cancellationToken);
+		return NoContent();
+	}
+}
